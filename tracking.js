e66cdd244aa3382ada41009a3610804c74a4a46b
LB123658 <79328251+LB123658@users.noreply.github.com>
2022-02-10 19:21:38 -0600

    Update tracking.js

--- tracking.js
+++ tracking.js
@@ -3,6 +3,6 @@ var pageTitle = document.getElementsByTagName("title")[0].innerHTML;
 var d = new Date();
 const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-var userData = pageTitle + " opened at " + d.toLocaleTimeString() + " " + days[d.getDay()] + ", " + months[d.getMonth()] + " " + d.getDay() + "<br><br>";
+var userData = pageTitle + " opened at " + d.toLocaleTimeString() + " " + days[d.getDay()] + ", " + months[d.getMonth()] + " " + d.getDate() + "<br><br>";
 localStorage.setItem("music_data",userData + localStorage.getItem("music_data"));
 console.log("Time and page recorded");
